b84617172b04d186d5d4b88e03cf484270e0d16e
Sayan Naskar <nascarsayan@gmail.com>
2024-06-18 13:45:44 +0530

    q4, q5, q6

--- day-2.sql
+++ day-2.sql
@@ -36,3 +36,54 @@ select * from product, printer;
 
 select * from product left outer join printer
 on product.model = printer.model;
+
+/*
+ Q4 Find all records from the Printer table
+ containing data about color printers.
+ */
+
+ select * from printer
+ where color = 'y';
+
+/*
+ Q5
+ Find the model number, speed and hard drive capacity of PCs
+ cheaper than $600 having a 12x or a 24x CD drive.
+ */
+
+select distinct model, speed, hd from pc
+where
+    price < 600
+    and
+    (cd = '12x' or cd = '24x');
+
+/*
+ Q6
+ For each maker producing laptops
+ with a hard drive capacity of 10 Gb or higher,
+ find the speed of such laptops.
+ Result set: maker, speed.
+
+ P1: we need to filter using maker
+   + maker should produce at least 1 Laptop with
+     a hard drive capacity of 10 Gb or higher
+
+ P2: we need to fetch all the laptops and their makers,
+    and filter using P1
+ */
+
+# table1 - product ; table2 - Laptop
+
+with makers_with_10gb_laptops as (
+select distinct maker from product
+where model in
+(select model
+ from laptop
+ where hd >= 10
+)), laptop_and_makers_10gb as (
+    select product.model, maker, speed from
+    laptop inner join product
+    on laptop.model = product.model
+    where hd >= 10
+) select distinct maker, speed from laptop_and_makers_10gb
+where maker in (select * from makers_with_10gb_laptops);
